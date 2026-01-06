9236ce628ade28664a680593a6323e990ed6a3b7
MiguelSenHer <miguel.senovilla@hotmail.es>
2026-01-06 18:11:33 +0100

    added view for assembly

--- config/frontend/views.py
+++ config/frontend/views.py
@@ -4,3 +4,5 @@ from django.shortcuts import render
 def home(request):
     return render(request, "frontend/template.html")
 
+def assembly(request):
+    return render(request, "frontend/assembly.html")
