952efc0a099085e28f3dc34a5c2aea04dfc261a9
cherifseddik <cherif.seddik2002@gmail.com>
2026-01-06 17:07:48 +0100

    added apps : accounts, plasmids, campaigns

--- config/config/urls.py
+++ config/config/urls.py
@@ -20,4 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("frontend.urls")),
+    path("accounts/", include("accounts.urls")),
+    path("plasmids/", include("plasmids.urls")),
+    path("campaigns/", include("campaigns.urls")),
 ]
---
+++ config/plasmids/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PlasmidsConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "plasmids"
