3ed25cf5c4bc1b8aaf1e361b9981a4c6fdecf599
MiguelSenHer <miguel.senovilla@hotmail.es>
2026-01-06 19:04:56 +0100

    browse vue dasn le bon endroit

--- config/frontend/views.py
+++ config/frontend/views.py
@@ -4,5 +4,4 @@ from django.shortcuts import render
 def home(request):
     return render(request, "frontend/template.html")
 
-def assembly(request):
-    return render(request, "frontend/assembly.html")
+
