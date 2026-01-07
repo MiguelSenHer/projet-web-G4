3da8be40710deea75aa4a10b188982c81000db16
MiguelSenHer <miguel.senovilla@hotmail.es>
2026-01-07 12:20:30 +0100

    models for Assembly, InputParts and Type

--- config/campaigns/models.py
+++ config/campaigns/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Assembly(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    comment = models.TextField()
+    creation_date = models.DateTimeField("Created At")
+    separator = models.CharField(max_length=20)
+    restriction_enzyme = models.CharField(max_length=50)
+    input_parts = models.ManyToManyField('InputParts', related_name='assemblies')
+
+
+class InputParts(models.Model):
+    assembly = models.ForeignKey(Assembly, on_delete=models.CASCADE)
+    part_name = models.CharField(max_length=200)
+    allowed_types = models.ManyToManyField('Type', related_name='input_parts')
+    optional = models.BooleanField(default=False)
+    mandatory = models.BooleanField(default=True)
+    separator = models.CharField(max_length=20)
+
+class Type(models.Model):
+    part = models.ForeignKey(InputParts, on_delete=models.CASCADE)
+    type_name = models.CharField(max_length=50)
+    
